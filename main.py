769bdf234aea7b1490d60f756673422c1739374f
Alejandrodd <94106519+Alejandrodd@users.noreply.github.com>
2022-02-15 13:38:46 +0100

    Update main.py

--- main.py
+++ main.py
@@ -11,6 +11,19 @@ screen_width=800
 screen_height=600
 screen=pygame.display.set_mode((screen_width, screen_height))
 
+#FONDO
+dimensiones=(800,600)
+screen=pygame.display.set_mode(dimensiones)
+pygame.display.set_caption("Mi Juego")
+pygame.display.flip()
+
+imagen1 = pygame.image.load('img fondo.png')
+punto1 = (0,0)
+screen.blit(imagen1, punto1)
+pygame.display.flip()
+
+a=input()
+
 # Color
 black=(0, 0, 0)
 white=(255, 255, 255)
