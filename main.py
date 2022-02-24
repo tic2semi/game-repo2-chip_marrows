82fa38126b702550fe9a7fa731fa19bc826cbdfd
Aestdom <94107190+Aestdom@users.noreply.github.com>
2022-02-24 10:06:37 +0100

    Main actualizado.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ pygame.mixer.pre_init(frequency=22050, size=-16, channels=2, buffer=4096)
 pygame.init()
 # Center the Game Application
 os.environ['SDL_VIDEO_CENTERED']='1'
-
+clock = pygame.time.Clock()
 # Resolucion
 screen_width=800
 screen_height=600
@@ -22,7 +22,7 @@ punto1 = (0,0)
 screen.blit(imagen1, punto1)
 pygame.display.flip()
 
-a=input()
+
 
 # Color
 black=(0, 0, 0)
@@ -35,16 +35,24 @@ lineWidth=3
 def BackgroundGameplay():
     screen.fill(black)
     pygame.draw.rect(screen, blue, ((0, 0), (screen_width, screen_height)), lineWidth)
-
+    screen.blit(imagen1, punto1)
     pygame.display.flip()
 #######################################################
 BackgroundGameplay()
 pygame.mouse.set_visible(3)
 
-while True:
-        for event in pygame.event.get():
-            if event.type==QUIT:
-                pygame.quit()
-                sys.exit()
+continuar=True
+while continuar:
+                     
+    for event in pygame.event.get():
+       if event.type==QUIT or event.type==pygame.KEYDOWN:
+            continuar=False
+            
+      
+
+    clock.tick(60)
+    
+pygame.quit()
+sys.exit();
 
 
